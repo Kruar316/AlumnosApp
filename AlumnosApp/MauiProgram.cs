b13ee5405ab411c2b297a1d7b7f82241a4849462
Talleres Puente Alto <tallerespa@inacapmail.cl>
2024-12-11 18:39:21 -0300

    Carga_Archivos

---
+++ AlumnosApp/MauiProgram.cs
@@ -0,0 +1,42 @@
+﻿using AlumnosApp.Modelos;
+using Firebase.Database;
+using Firebase.Database.Query;
+using Microsoft.Extensions.Logging;
+
+namespace AlumnosApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+            Registrar();
+            return builder.Build();
+        }
+
+        public static void Registrar()
+        {
+            FirebaseClient client = new FirebaseClient("https://registroalumnos-8406d-default-rtdb.firebaseio.com/");
+
+            var cursos = client.Child("Cursos").OnceAsync<Curso>();
+
+            if (cursos.Result.Count == 0)
+            {
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "1° Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "2° Basico" });
+                client.Child("Cursos").PostAsync(new Curso { Nombre = "3° Basico" });
+            }
+        }
+    }
+}
